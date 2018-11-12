f3bfc06b99c312f6a2d7de741ce2602c6011b173
ttinies <ttinies.v@gmail.com>
2018-11-12 12:46:45 -0700

    - relocated globally available gameloop and loops since gameloop update to the common package

---
+++ sc2common/variables.py
@@ -0,0 +1,8 @@
+
+"""
+this module contains globals which change frequently
+"""
+
+gameloop        = 0     # keep track of current gameloop
+gameloopDelta   = 0     # keep track of number of elapsed gameloops since last update
+
